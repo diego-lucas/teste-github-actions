00e2b440f03ce6f91b1ddbe4c87947a95c8c59db
Diego Lucas <diegolucas.natal@gmail.com>
2025-04-22 22:49:01 -0300

    adicionando black

--- lambda_function.py
+++ lambda_function.py
@@ -5,12 +5,6 @@ import random
 def lambda_handler(event, context):
     num = random.randint(1, 5)
     if num == 5:
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Erro!')
-        }
+        return {"statusCode": 500, "body": json.dumps("Erro!")}
     else:
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Sucesso!')
-        }
+        return {"statusCode": 200, "body": json.dumps("Sucesso!")}
