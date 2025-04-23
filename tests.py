4c0dd7eb275a4504bee659ae4d12ba06bbb85f24
Diego Lucas <diegolucas.natal@gmail.com>
2025-04-23 12:54:04 -0300

    adicionando testes

---
+++ tests.py
@@ -0,0 +1,15 @@
+import json
+from lambda_function import lambda_handler
+import pytest
+
+def test_lambda_success(monkeypatch):
+    monkeypatch.setattr("random.randint", lambda a, b: 1)
+    resp = lambda_handler({}, {})
+    assert resp["statusCode"] == 200
+    assert json.loads(resp["body"]) == "Sucesso!"
+
+def test_lambda_error(monkeypatch):
+    monkeypatch.setattr("random.randint", lambda a, b: 5)
+    resp = lambda_handler({}, {})
+    assert resp["statusCode"] == 500
+    assert json.loads(resp["body"]) == "Erro!"
