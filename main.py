637036de8244703e13789e02527260dbbdd00a54
Diego Lucas <diegolucas.natal@gmail.com>
2025-04-22 22:15:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+import random
+
+
+def lambda_handler(event, context):
+    num = random.randint(1, 5)
+    if num == 5:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Erro!')
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Sucesso!')
+        }
